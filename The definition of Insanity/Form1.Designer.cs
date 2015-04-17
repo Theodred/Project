84e07cdacc7392a082c79cd5f2b8ca0ac7ed4711
Teo <theodenistired@yahoo.com>
2015-04-17 21:22:08 +0300

    Mallttx

---
+++ The definition of Insanity/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace The_definition_of_Insanity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtb_Message = new System.Windows.Forms.RichTextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.label_Connect = new System.Windows.Forms.Label();
+            this.tb_LocalIP = new System.Windows.Forms.TextBox();
+            this.tb_LocalPort = new System.Windows.Forms.TextBox();
+            this.tb_RemotePort = new System.Windows.Forms.TextBox();
+            this.tb_RemoteIP = new System.Windows.Forms.TextBox();
+            this.lb_Conversation = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_LocalPort);
+            this.groupBox1.Controls.Add(this.tb_LocalIP);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_RemotePort);
+            this.groupBox2.Controls.Add(this.tb_RemoteIP);
+            this.groupBox2.Location = new System.Drawing.Point(435, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // rtb_Message
+            // 
+            this.rtb_Message.Location = new System.Drawing.Point(13, 370);
+            this.rtb_Message.Name = "rtb_Message";
+            this.rtb_Message.Size = new System.Drawing.Size(512, 60);
+            this.rtb_Message.TabIndex = 3;
+            this.rtb_Message.Text = "";
+            this.rtb_Message.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtb_Message_KeyPress);
+            // 
+            // Send
+            // 
+            this.Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Send.Location = new System.Drawing.Point(531, 370);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(104, 60);
+            this.Send.TabIndex = 4;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // label_Connect
+            // 
+            this.label_Connect.AutoSize = true;
+            this.label_Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Connect.Location = new System.Drawing.Point(218, 40);
+            this.label_Connect.Name = "label_Connect";
+            this.label_Connect.Size = new System.Drawing.Size(211, 55);
+            this.label_Connect.TabIndex = 5;
+            this.label_Connect.Text = "Connect";
+            this.label_Connect.Click += new System.EventHandler(this.label_Connect_Click);
+            // 
+            // tb_LocalIP
+            // 
+            this.tb_LocalIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_LocalIP.Location = new System.Drawing.Point(7, 20);
+            this.tb_LocalIP.Name = "tb_LocalIP";
+            this.tb_LocalIP.Size = new System.Drawing.Size(187, 31);
+            this.tb_LocalIP.TabIndex = 0;
+            // 
+            // tb_LocalPort
+            // 
+            this.tb_LocalPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_LocalPort.Location = new System.Drawing.Point(7, 57);
+            this.tb_LocalPort.Name = "tb_LocalPort";
+            this.tb_LocalPort.Size = new System.Drawing.Size(187, 31);
+            this.tb_LocalPort.TabIndex = 1;
+            // 
+            // tb_RemotePort
+            // 
+            this.tb_RemotePort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_RemotePort.Location = new System.Drawing.Point(6, 57);
+            this.tb_RemotePort.Name = "tb_RemotePort";
+            this.tb_RemotePort.Size = new System.Drawing.Size(187, 31);
+            this.tb_RemotePort.TabIndex = 2;
+            // 
+            // tb_RemoteIP
+            // 
+            this.tb_RemoteIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_RemoteIP.Location = new System.Drawing.Point(6, 20);
+            this.tb_RemoteIP.Name = "tb_RemoteIP";
+            this.tb_RemoteIP.Size = new System.Drawing.Size(187, 31);
+            this.tb_RemoteIP.TabIndex = 3;
+            // 
+            // lb_Conversation
+            // 
+            this.lb_Conversation.FormattingEnabled = true;
+            this.lb_Conversation.Location = new System.Drawing.Point(13, 119);
+            this.lb_Conversation.Name = "lb_Conversation";
+            this.lb_Conversation.Size = new System.Drawing.Size(615, 238);
+            this.lb_Conversation.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 442);
+            this.Controls.Add(this.lb_Conversation);
+            this.Controls.Add(this.label_Connect);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.rtb_Message);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tb_LocalPort;
+        private System.Windows.Forms.TextBox tb_LocalIP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tb_RemotePort;
+        private System.Windows.Forms.TextBox tb_RemoteIP;
+        private System.Windows.Forms.RichTextBox rtb_Message;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Label label_Connect;
+        private System.Windows.Forms.ListBox lb_Conversation;
+    }
+}
+
---
+++ The definition of Insanity/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace The_definition_of_Insanity
+{
+    public partial class Form1 : Form
+    {
+        Socket socket;
+        EndPoint epLocal, epRemote;
+        byte[] buffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            tb_LocalIP.Text = GetLocalIP();
+            tb_RemoteIP.Text = GetLocalIP();
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+
+            return "127.0.0.1";
+        }
+
+        private void label_Connect_Click(object sender, EventArgs e)
+        {
+            epLocal = new IPEndPoint(IPAddress.Parse(tb_LocalIP.Text), Convert.ToInt32(tb_LocalPort.Text));
+            socket.Bind(epLocal);
+
+            epRemote = new IPEndPoint(IPAddress.Parse(tb_RemoteIP.Text), Convert.ToInt32(tb_RemotePort.Text));
+            socket.Connect(epRemote);
+
+            buffer = new byte[1500];
+            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receivedData = new byte[1500];
+                receivedData = (byte[])aResult.AsyncState;
+
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                string receivedMessage = aEncoding.GetString(receivedData);
+
+                lb_Conversation.Items.Add("\rFriend: " + receivedMessage);
+
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding aEncoding = new ASCIIEncoding();
+            byte[] sendingMessage = new byte[1500];
+            sendingMessage = aEncoding.GetBytes(rtb_Message.Text);
+
+            socket.Send(sendingMessage);
+
+            lb_Conversation.Items.Add("\rMe: " + rtb_Message.Text);
+            rtb_Message.Text = "";
+        }
+
+        private void rtb_Message_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch(e.KeyChar)
+            {
+                case (char)Keys.Enter:
+                    ASCIIEncoding aEncoding = new ASCIIEncoding();
+                    byte[] sendingMessage = new byte[1500];
+                    sendingMessage = aEncoding.GetBytes(rtb_Message.Text);
+
+                    socket.Send(sendingMessage);
+
+                    lb_Conversation.Items.Add("\rMe: " + rtb_Message.Text);
+                    rtb_Message.Text = "";
+                    break;
+            }
+        }
+    }
+}
